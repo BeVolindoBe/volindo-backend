bdb603d866028aabd96a78e9bbec421970e89f4f
jpchv <juanpablo@volindo.com>
2022-11-15 15:00:35 -0600

    ADD: swagger documentation

--- src/traveler/views.py
+++ src/traveler/views.py
@@ -8,12 +8,5 @@ from traveler.models import Traveler
 
 class TravelerDetail(RetrieveAPIView):
 
-    def get(self, request, agent_id, traveler_id):
-        try:
-            traveler = Traveler.objects.get(agent_id=agent_id, id=traveler_id)
-            return Response(TravelerSerializer(traveler).data, status=status.HTTP_200_OK)
-        except Traveler.DoesNotExist:
-            return Response(
-                {'error': '404', 'message': 'traveler not found'},
-                status=status.HTTP_404_NOT_FOUND
-            )
+    serializer_class = TravelerSerializer
+    queryset = Traveler.objects.all()
--- src/volindoapp/urls.py
+++ src/volindoapp/urls.py
@@ -1,4 +1,8 @@
-from django.urls import path, include
+from django.conf import settings
+
+from django.urls import path, include, re_path
+
+from rest_framework import permissions
 
 
 urlpatterns = [
@@ -6,4 +10,23 @@ urlpatterns = [
     path('agents/<str:agent_id>/travelers/', include('traveler.urls')),
     path('agents/', include('agent.urls')),
     path('hotels/', include('hotel.urls')),
+    
 ]
+
+if settings.DEBUG:
+
+    from drf_yasg.views import get_schema_view
+    from drf_yasg import openapi
+
+    schema_view = get_schema_view(
+       openapi.Info(
+          title="Volindo API",
+          default_version='v1',
+          description="Volindo API",
+       ),
+       public=True,
+       permission_classes=[permissions.AllowAny],
+    )
+
+    urlpatterns += [re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')]
+    urlpatterns += [re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')]
