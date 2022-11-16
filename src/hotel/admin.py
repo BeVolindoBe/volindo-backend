029f99ac16536faa619ce6e70dfea8a294343d53
juanpablo <mrjpch@outlook.com>
2022-11-16 12:44:40 -0600

    UPDATE: hotels admin filters

--- src/hotel/admin.py
+++ src/hotel/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from catalogue.models import Item
+
 from hotel.models import Hotel, HotelPicture, HotelAmenity
 
 
@@ -10,6 +12,13 @@ class HotelAdmin(admin.ModelAdmin):
         'stars'
     ]
     search_fields = ['hotel_name', ]
+
+    def render_change_form(self, request, context, *args, **kwargs):
+        context['adminform'].form.fields['destination'].queryset = Item.objects.filter(
+            catalogue__slug='destinations'
+        )
+        return super(HotelAdmin, self).render_change_form(request, context, *args, **kwargs)
+
 
 
 admin.site.register(HotelPicture)
