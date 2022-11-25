593b2e87750acf4e5afeb34f5a789c9358f996b5
juanpablo <juanpablo@volindo.com>
2022-11-25 10:57:03 -0600

    FIX: payment link url

--- payment/urls.py
+++ payment/urls.py
@@ -4,6 +4,6 @@ from payment.views import NewExternalAgent, payment_link
 
 
 urlpatterns = [
-    path('<str:payment_id/', payment_link),
     path('external_agents/', NewExternalAgent.as_view()),
+    path('<str:payment_id>/', payment_link),
 ]
