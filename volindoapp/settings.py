47d4cdbb257492114d25c18f1bdbbc4097711e36
juanpablo <juanpablo@volindo.com>
2022-11-28 16:01:14 -0600

    UPDATE: allowed hosts settings

--- volindoapp/settings.py
+++ volindoapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if environ.get('DEBUG') == 'True' else False
 
-ALLOWED_HOSTS = [environ.get('ALLOWED_HOSTS').split(','), ]
+ALLOWED_HOSTS = environ.get('ALLOWED_HOSTS').split(',')
 
 # Application definition
 
