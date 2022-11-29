3288aa5ba323c0f07a713478ca438f0f61cd641e
juanpablo <juanpablo@volindo.com>
2022-11-29 17:48:51 -0600

    UPDATE: commision field

--- payment/models.py
+++ payment/models.py
@@ -15,7 +15,7 @@ class ReservationPayment(models.Model):
         related_name='payments'
     )
     amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    commission = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+    commission = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     approved_at = models.DateTimeField(null=True, default=None)
     created_at = models.DateTimeField(auto_now_add=True)
