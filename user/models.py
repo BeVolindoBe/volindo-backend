d694a78c3c88528d5d6dc0025b7913f87a7aa2b5
juanpablo <juanpablo@volindo.com>
2023-01-09 21:27:26 -0600

    UPDATE: user model

--- user/models.py
+++ user/models.py
@@ -1,12 +1,10 @@
-from uuid import uuid4
-
 from django.db import models
 
 from catalogue.models import Country
 
 
 class User(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, editable=False)
     full_name = models.CharField(max_length=200, default=None)
     email = models.EmailField(unique=True)
     updated_at = models.DateTimeField(auto_now=True)
